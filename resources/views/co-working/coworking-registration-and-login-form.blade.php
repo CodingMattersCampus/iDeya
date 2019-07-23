3b1fc31cdbb86df95cf09d75e956e5663f6bbfaf
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-07-23 15:21:55 +0800

    removed log-in fields

--- resources/views/co-working/coworking-registration-and-login-form.blade.php
+++ resources/views/co-working/coworking-registration-and-login-form.blade.php
@@ -1,52 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header" align="Left"><h3>Login as Co-working</h3>
-                        <form class="form-inline" method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <div class="form-row">
-                                <div class="form-group col-md-4">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-                                    <input type="email" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" value="{{ old('coworking-email') }}" required autofocus id="coworking-email" placeholder="Enter email" name="coworking-email">
-                                        @if ($errors->has('coworking-email'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('coworking-email') }}</strong>
-                                        </span>
-                                         @endif
-                                </div>
-
-                                <div class="form-group col-md-4">
-                                <label for="password" class="col-md-4 col-form-label text-md-right"> {{ __('Password') }}</label>
-                                    <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" placeholder="Enter password" name="password" required>
-                                        @if ($errors->has('password'))
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                        @endif
-                                </div>
-
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary" id="login">{{ __('Login') }}</button>
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-
-                            </div>
-                </div>
-            </div>
-        </div>
-    </div>
-        </form><br>
-
-
                  <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-md-14">
