f93447912912c59a5cd7d4de61086243764069c7
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-08-09 15:49:09 +0800

    added coworking attendance model

---
+++ modules/Office/Entities/CoWorkingAttendance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Office\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CoWorkingAttendance extends Model
+{
+    protected $guarded = [];
+     public function fullName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+}
