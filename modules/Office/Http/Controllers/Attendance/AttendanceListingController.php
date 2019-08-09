6fdd031f64cd80a7531b41b707d9cc2dc09a2b87
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-08-09 15:46:59 +0800

    added coworking function

--- modules/Office/Http/Controllers/Attendance/AttendanceListingController.php
+++ modules/Office/Http/Controllers/Attendance/AttendanceListingController.php
@@ -5,6 +5,7 @@ namespace Modules\Office\Http\Controllers\Attendance;
 use Modules\Office\Entities\GuestAttendance;
 use Modules\Office\Entities\EmployeeAttendance;
 use Modules\Office\Entities\InternAttendance;
+use Modules\Office\Entities\CoworkingAttendance;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
@@ -17,6 +18,7 @@ class AttendanceListingController extends Controller
         $guestAttendance = GuestAttendance::all();
         $employeeAttendance = EmployeeAttendance::all();
         $internAttendance = InternAttendance::all();
+        $coworkingAttendance = CoworkingAttendance::all();
      
 
         return view('office::attendance.listing', compact('guestAttendance','employeeAttendance','internAttendance'));
